ac286c62fb32664780318387396b503f8a39163f
tordn <tordfnytun@gmail.com>
2023-04-18 21:19:33 +0200

    Add appfactory that initalizes db and registers blueprints

--- empower/__init__.py
+++ empower/__init__.py
@@ -3,14 +3,21 @@ import os
 from flask import Flask
 
 
+# appfactory
 def create_app(test_config=None):
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'empower.sqlite'),
     )
 
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
@@ -24,9 +31,14 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    # registering db functions with the application
+    from . import db
+    db.init_app(app)
+
+    # registering the blueprints
+    from . import transformer, home
+    app.register_blueprint(transformer.bp)
+    app.register_blueprint(home.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
