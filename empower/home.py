4c73e9ea7cae61af0152dba152aa31ee4303424b
tordn <tordfnytun@gmail.com>
2023-04-20 19:39:52 +0200

    Add bool method to check if sqlite databse has entries of Nord Energi and posts bool result to the index page

--- empower/home.py
+++ empower/home.py
@@ -6,6 +6,19 @@ bp = Blueprint('home', __name__)
 
 @bp.route('/')
 def index():
-    db = get_db()
-
     return render_template('home/index.html')
+
+
+@bp.before_app_request
+def db_populated():
+    db = get_db()
+    provider = "Nord Energi"
+    test_db_query = db.execute(
+        'SELECT * FROM plans WHERE provider = ?', (provider,)
+    ).fetchall()
+
+    # if the fetchall is an empty list, the table "plans" has no rows with Nord Energi
+    if len(test_db_query) == 0:
+        return False
+    else:
+        return True
